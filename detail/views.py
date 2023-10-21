4429c16254168c5a83235c53c9fa2ba0f950d0ec
Yacn1414 <yacn1214@gmail.com>
2023-10-21 14:59:36 +0330

    detail

--- detail/views.py
+++ detail/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-
+from core.models import posts,cat,Site,Socials
 # Create your views here.
 def search(request):
     pass
 def detail_post(request,slug):
-    pass
+    data = posts.objects.get(slug=slug)
+    catd = cat.objects.all()
+    Sited = Site.objects.first()
+    postss = posts.objects.order_by('-views')[:10]
+    postsd = posts.objects.order_by('-views')[:3]
+    Socialsd = Socials.objects.all()
+    return render(request,'detail.html',{
+        "cat":catd,
+        "Site":Sited,
+        "post":postsd,
+        "data":data,
+        "postss":postss,
+        "social":Socialsd,
+    })
 def category(request,slug):
     pass
